8f3f23b6ef55cb40430e7207b00842be3f6a850b
evansdad <nktang@163.com>
2022-03-23 22:04:33 +0800

    chapter 06

--- ssia-ch6-ex1/src/main/resources/schema.sql
+++ ssia-ch6-ex1/src/main/resources/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS `spring`.`user` (
+CREATE TABLE IF NOT EXISTS `security-in-action`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` TEXT NOT NULL,
   `algorithm` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `spring`.`authority` (
+CREATE TABLE IF NOT EXISTS `security-in-action`.`authority` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `user` INT NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `spring`.`product` (
+CREATE TABLE IF NOT EXISTS `security-in-action`.`product` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `price` VARCHAR(45) NOT NULL,
