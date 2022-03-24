4b603eb9f5a4d479f5dd59dc74c538f6429c0a9b
evansdad <nktang@163.com>
2022-03-24 17:04:44 +0800

    chapter 14

--- ssia-ch11-ex1-s1/src/main/resources/schema.sql
+++ ssia-ch11-ex1-s1/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `security-in-action-11`.`user` (
+CREATE TABLE IF NOT EXISTS `security-in-action`.`user` (
     `username` VARCHAR(45) NOT NULL,
     `password` TEXT NULL,
     PRIMARY KEY (`username`));
 
-CREATE TABLE IF NOT EXISTS `security-in-action-11`.`otp` (
+CREATE TABLE IF NOT EXISTS `security-in-action`.`otp` (
     `username` VARCHAR(45) NOT NULL,
     `code` VARCHAR(45) NULL,
     PRIMARY KEY (`username`));
