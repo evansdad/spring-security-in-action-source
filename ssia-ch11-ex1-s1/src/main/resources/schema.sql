1b1e0a134b1c158a72eb651fcf715e3d920c937b
evansdad <nktang@163.com>
2022-03-24 15:04:33 +0800

    chapter11 mod

--- ssia-ch11-ex1-s1/src/main/resources/schema.sql
+++ ssia-ch11-ex1-s1/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `spring`.`user` (
-    `username` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `security-in-action-11`.`user` (
+    `username` VARCHAR(45) NOT NULL,
     `password` TEXT NULL,
     PRIMARY KEY (`username`));
 
-CREATE TABLE IF NOT EXISTS `spring`.`otp` (
+CREATE TABLE IF NOT EXISTS `security-in-action-11`.`otp` (
     `username` VARCHAR(45) NOT NULL,
     `code` VARCHAR(45) NULL,
     PRIMARY KEY (`username`));
