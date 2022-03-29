3a96fccb52fecf46e29d360c201aeccbeeb4b556
evansdad <nktang@163.com>
2022-03-29 12:05:07 +0800

    ch18

--- ssia-ch17-ex5/src/main/resources/schema.sql
+++ ssia-ch17-ex5/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `spring`.`product` (
+CREATE TABLE IF NOT EXISTS `security-in-action`.`product` (
      `id` INT NOT NULL AUTO_INCREMENT,
      `name` VARCHAR(45) NULL,
      `owner` VARCHAR(45) NULL,
